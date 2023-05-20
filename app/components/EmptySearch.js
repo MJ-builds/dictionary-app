16e8fba1c4f87ba9db5edd295aec19470c342336
Michael <112978328+MJ-builds@users.noreply.github.com>
2023-05-20 20:49:38 +0200

    aesth.

--- app/components/EmptySearch.js
+++ app/components/EmptySearch.js
@@ -2,7 +2,7 @@
 
 export default function EmptySearch() {
   return (
-    <div className="flex w-full md:w-[737px] -mt-6">
+    <div className="flex w-full md:w-[737px] -mt-3 font-normal">
       <div className="w-[350px] md:w-full md:justify-center md:items-center text-[#FF5252] text-base md:text-xl">
         Whoops, can't be empty...
       </div>
